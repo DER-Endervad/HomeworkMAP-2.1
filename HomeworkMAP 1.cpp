8f7a69281f3382cb1bfe433eb9b51babc84940a8
Даниил <derendervad@gmail.com>
2023-05-21 15:42:53 +0300

    homework 2

--- HomeworkMAP 1.cpp
+++ HomeworkMAP 1.cpp
@@ -2,34 +2,36 @@
 #include <thread>
 #include <Windows.h>
 
-void clients(int& counter);
-void operationists(int& counter);
+void clients();
+void operationists();
+std::atomic<int> counter = 0;
+
 
 int main()
 {
     setlocale(LC_ALL, "Russian");
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-    int counter = 0;
-    std::thread t1(clients, std::ref(counter));
-    std::thread t2(operationists, std::ref(counter));
+    std::thread t1(clients);
+    std::thread t2(operationists);
     t1.join(); t2.join();
 }
 
-void clients(int& counter) {
+void clients() {
     while(counter < 10) {
         Sleep(1000);
-        counter++;
+        counter.exchange(counter + 1);
         std::cout << "Пришел клиент. Всего клиентов в очереди: " << counter << std::endl;
     }
     std::cout << "Очередь переполнена. Больше клиентов не принимаем." << std::endl;
 }
 
-void operationists(int& counter) {
+void operationists() {
     Sleep(2000);
     while (counter > 0) {
         Sleep(2000);
-        counter--;
+        counter.exchange(counter - 1);
+        /*counter--;*/
         std::cout << "Операция с клиентом завершено. Клиентов в очереди осталось: " << counter << std::endl;
     }
     std::cout << "Все операции с клиентами завершены. Рабочий день подошел к концу." << std::endl;
